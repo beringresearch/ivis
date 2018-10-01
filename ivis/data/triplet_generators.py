f9a9a96479a82054f51f6bcd3a4236bafecb58cb
Szubie <benszubert@gmail.com>
2018-10-01 15:06:28 +0100

    Add support for supervised dimensionality reduction when labels are supplied to the .fit method. New triplet generators draw triplets based on examples with the same labels.

--- ivis/data/triplet_generators.py
+++ ivis/data/triplet_generators.py
@@ -17,13 +17,16 @@ from .knn import build_annoy_index, extract_knn
 import numpy as np
 
 
-def create_triplet_generator(X, k, ntrees, batch_size, search_k=-1, precompute=True):
-    if precompute == True:
-        neighbour_list = extract_knn(X, k=k, ntrees=ntrees, search_k=search_k)
-        return generate_knn_triplets_from_neighbour_list(X, neighbour_list, batch_size=batch_size)
+def create_triplet_generator(X, k, ntrees, batch_size, search_k=-1, precompute=True, y=None):
+    if y is None:
+        if precompute == True:
+            neighbour_list = extract_knn(X, k=k, ntrees=ntrees, search_k=search_k)
+            return generate_knn_triplets_from_neighbour_list(X, neighbour_list, batch_size=batch_size)
+        else:
+            index = build_annoy_index(X, k=k, ntrees=ntrees)
+            return generate_knn_triplets_from_annoy_index(X, index, k=k, batch_size=batch_size, search_k=search_k)
     else:
-        index = build_annoy_index(X, k=k, ntrees=ntrees)
-        return generate_knn_triplets_from_annoy_index(X, index, k=k, batch_size=batch_size, search_k=search_k)
+        return generate_triplets_from_labels(X, y, batch_size=batch_size)
 
 
 def knn_triplet_from_neighbour_list(X, index, neighbour_list):
@@ -101,6 +104,46 @@ def generate_knn_triplets_from_annoy_index(X, annoy_index, k=150, batch_size=32,
         
         triplet_batch = np.array(triplet_batch)
         yield ([triplet_batch[:,0], triplet_batch[:,1], triplet_batch[:,2]], placeholder_labels)
+
+def generate_triplets_from_labels(X, Y, batch_size=32):
+    iterations = 0
+    row_indexes = np.array(list(range(len(X))), dtype=np.uint32)
+    np.random.shuffle(row_indexes)
+
+    placeholder_labels = np.array([0 for i in range(batch_size)])
+
+    while True:
+        triplet_batch = []
+        
+        for i in range(batch_size):
+            if iterations >= len(X):
+                np.random.shuffle(row_indexes)
+                iterations = 0
+           
+            triplet = triplet_from_labels(X, Y, row_indexes[iterations])
+            triplet_batch += triplet
+            iterations += 1
+        
+        triplet_batch = np.array(triplet_batch)
+        yield ([triplet_batch[:,0], triplet_batch[:,1], triplet_batch[:,2]], placeholder_labels)
+
+def triplet_from_labels(X, Y, index):
+    """ A random (unweighted) positive example chosen. """
+    triplets = []
+
+    row_label = Y[index]
+    neighbour_indexes = np.where(Y == row_label)[0]
+    
+    # Take a random neighbour as positive
+    neighbour_ind = np.random.choice(neighbour_indexes)
+    
+    # Take a random non-neighbour as negative
+    negative_ind = np.random.randint(0, len(X))     # Pick a random index until one fits constraint. An optimization.
+    while negative_ind in neighbour_indexes:
+        negative_ind = np.random.randint(0, len(X))
+    
+    triplets += [[X[index], X[neighbour_ind], X[negative_ind]]]
+    return triplets
 
 def create_triplets_from_positive_index_dict(X, positive_index_dict):
     triplets = []
