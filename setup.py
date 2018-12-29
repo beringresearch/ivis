0b9a2952ae6570f7edf0ec1da89c6918c7e422e8
Ignat Drozdov <idrozdov@beringresearch.com>
2018-12-29 20:49:38 +0000

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='ivis',
-      version='1.1',
+      version='1.1.1',
       description='Artificial neural network-driven visualization of high-dimensional data using triplets.',
       url='http://github.com/beringresearch/ivis',
       author='Benjamin Szubert, Ignat Drozdov',
