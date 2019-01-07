2c9923feaf098ca625d3d5fca6363122631cb9ad
Szubie <benszubert@gmail.com>
2019-01-07 09:53:33 +0000

    Add manhatten distance metric. Both the standard triplet loss and the modified pn-loss now support use of the manhatten distance metric.

--- ivis/nn/losses.py
+++ ivis/nn/losses.py
@@ -10,12 +10,17 @@ def triplet_loss(distance='pn', margin=1):
         'pn' : pn_loss(margin=margin),
         'euclidean' : euclidean_loss(margin=margin),
         'softmax_ratio' : softmax_ratio,
-        'softmax_ratio_pn' : softmax_ratio_pn
+        'softmax_ratio_pn' : softmax_ratio_pn,
+        'manhatten': manhatten_loss(margin=margin),
+        'manhatten_pn': manhatten_pn_loss(margin=margin)
     }
     return distances[distance]
 
 def _euclidean_distance(x, y):
     return K.sqrt(K.maximum(K.sum(K.square(x - y), axis=1, keepdims=True), K.epsilon()))
+
+def _manhatten_distance(x, y):
+    return K.maximum(K.sum(K.abs(x - y), axis=1, keepdims=True), K.epsilon())
 
 def pn_loss(margin=1):
     def _pn_loss(y_true, y_pred):    
@@ -36,6 +41,26 @@ def euclidean_loss(margin=1):
         anchor, positive, negative = tf.unstack(y_pred)        
         return K.mean(K.maximum(_euclidean_distance(anchor, positive) - _euclidean_distance(anchor, negative) + margin, 0))
     return _euclidean_loss
+
+def manhatten_loss(margin=1):
+    def _manhatten_loss(y_true, y_pred):
+        anchor, positive, negative = tf.unstack(y_pred)        
+        return K.mean(K.maximum(_manhatten_distance(anchor, positive) - _manhatten_distance(anchor, negative) + margin, 0))
+    return _manhatten_loss
+
+def manhatten_pn_loss(margin=1):
+    def _pn_loss(y_true, y_pred):    
+        anchor, positive, negative = tf.unstack(y_pred)
+
+        anchor_positive_distance = _manhatten_distance(anchor, positive)
+        anchor_negative_distance = _manhatten_distance(anchor, negative)
+        positive_negative_distance = _manhatten_distance(positive, negative)
+
+        minimum_distance = K.min(K.concatenate([anchor_negative_distance, positive_negative_distance]), axis=1, keepdims=True)
+
+        return K.mean(K.maximum(anchor_positive_distance - minimum_distance + margin, 0))
+    
+    return _pn_loss 
 
 def softmax_ratio(y_true, y_pred):
     anchor, positive, negative = tf.unstack(y_pred)
