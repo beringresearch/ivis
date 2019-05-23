812e22e4aef9893f809ebfb0bbcd8915ba00887d
Ignat Drozdov <idrozdov@beringresearch.com>
2019-05-23 16:51:23 +0100

    explicity save/load example

--- examples/iris.py
+++ examples/iris.py
@@ -5,6 +5,9 @@ from ivis import Ivis
 X = load_iris().data
 X = MinMaxScaler().fit_transform(X)
 
-embeddings = Ivis(k=3, batch_size=120).fit_transform(X)
+ivis = Ivis(k=3, batch_size=120)
+ivis.fit(X)
+embeddings = ivis.transform(X)
 
+ivis.save_model('iris.ivis')
 
