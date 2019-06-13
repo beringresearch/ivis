38d308e87e58679c0c82131f2172d857901ff40a
Ignat Drozdov <ignat@Ignats-MacBook-Pro.local>
2019-06-13 13:20:49 +0100

    versino control annoy

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='ivis',
-      version='1.1.3',
+      version='1.1.4',
       description='Artificial neural network-driven visualization of high-dimensional data using triplets.',
       url='http://github.com/beringresearch/ivis',
       author='Benjamin Szubert, Ignat Drozdov',
@@ -14,7 +14,7 @@ setup(name='ivis',
           'keras',
           'numpy',
           'scikit-learn',
-          'annoy',
+          'annoy>=1.15.2',
           'tqdm'
       ],
       extras_require={
