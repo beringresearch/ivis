f21c59903fa88fba572feb66f2f87b7134b7fa7a
idroz <idrozdov@beringresearch.com>
2019-06-26 19:20:12 +0100

    feature version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='ivis',
-      version='1.1.5',
+      version='1.2.0',
       description='Artificial neural network-driven visualization of high-dimensional data using triplets.',
       long_description=long_description,
       long_description_content_type='text/markdown',
