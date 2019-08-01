c46f45ca069b36040998a0d911b548c42e45b750
Szubie <benszubert@gmail.com>
2019-08-01 12:48:40 +0100

    Execute Travis R package installation commands as sudo

--- ci_scripts/install-R.sh
+++ ci_scripts/install-R.sh
@@ -1,3 +1,3 @@
-Rscript -e 'install.packages("reticulate")'
-Rscript -e 'install.packages("devtools")'
-Rscript -e 'devtools::install_github("beringresearch/ivis/R-package");library(ivis);install_ivis()'
+sudo Rscript -e 'install.packages("reticulate")'
+sudo Rscript -e 'install.packages("devtools")'
+sudo Rscript -e 'devtools::install_github("beringresearch/ivis/R-package");library(ivis);install_ivis()'
