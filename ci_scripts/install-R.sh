5cf54521770afbdb16cac1f843445e373f46043c
Szubie <benszubert@gmail.com>
2019-08-01 12:35:29 +0100

    Add travis R scripts

---
+++ ci_scripts/install-R.sh
@@ -0,0 +1,3 @@
+Rscript -e 'install.packages("reticulate")'
+Rscript -e 'install.packages("devtools")'
+Rscript -e 'devtools::install_github("beringresearch/ivis/R-package");library(ivis);install_ivis()'
