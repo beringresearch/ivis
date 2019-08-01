42b6228866db790de1fae05dfb1cac2c42412e1d
Szubie <benszubert@gmail.com>
2019-08-01 12:59:09 +0100

    Add mirror to R package installation. Install python venv.

--- ci_scripts/install-R.sh
+++ ci_scripts/install-R.sh
@@ -1,3 +1,6 @@
-sudo Rscript -e 'install.packages("reticulate")'
-sudo Rscript -e 'install.packages("devtools")'
+sudo Rscript -e 'install.packages("reticulate", repos="http://cran.us.r-project.org")'
+sudo Rscript -e 'install.packages("devtools"), repos="http://cran.us.r-project.org"'
+
+python3 -m pip install --user virtualenv 
+
 sudo Rscript -e 'devtools::install_github("beringresearch/ivis/R-package");library(ivis);install_ivis()'
