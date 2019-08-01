0db3bda68c2eb011566cf292edaa09fbc8188f91
idroz <idrozdov@beringresearch.com>
2019-08-01 13:28:19 +0100

    drop r version and user for travis

--- ci_scripts/install-R.sh
+++ ci_scripts/install-R.sh
@@ -1,6 +1,6 @@
 sudo Rscript -e 'install.packages("reticulate", repos="http://cran.us.r-project.org")'
 sudo Rscript -e 'install.packages("devtools", repos="http://cran.us.r-project.org")'
 
-python3 -m pip install --user virtualenv 
+python3 -m pip install virtualenv 
 
 sudo Rscript -e 'devtools::install_github("beringresearch/ivis/R-package");library(ivis);install_ivis()'
