0aae9bb1abbb72a1ad7cfea87b42e547550c0dc9
Szubie <benszubert@gmail.com>
2019-08-01 17:11:43 +0100

    Add python-setuptools to Travis Python3 env

--- ci_scripts/install-R.sh
+++ ci_scripts/install-R.sh
@@ -1,10 +1,3 @@
-deactivate
 sudo Rscript -e 'remotes::install_github("rstudio/reticulate")'
 sudo Rscript -e 'install.packages("devtools", repos="http://cran.us.r-project.org")'
-
-sudo apt-get install python3-pip
-python3 -m pip install --user --upgrade pip
-python3 -m pip install --user virtualenv 
-python3 -m pip install numpy
-
 sudo Rscript -e 'devtools::install_github("beringresearch/ivis/R-package");library(ivis);install_ivis()'
--- ci_scripts/install.sh
+++ ci_scripts/install.sh
@@ -1,6 +1,2 @@
-deactivate
-sudo apt-get install python3-pip
-python3 -m pip install --user --upgrade pip
-
 pip install -e .
 pip install pytest
