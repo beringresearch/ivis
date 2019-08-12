481924abb592476f32aa97e0fdf8f49e220c2638
Szubie <benszubert@gmail.com>
2019-08-12 09:40:17 +0100

    Updated classification probability method name to "score_samples" in line with other examples in the sklearn API

--- tests/integration/test_supervised.py
+++ tests/integration/test_supervised.py
@@ -4,7 +4,7 @@ import numpy as np
 import pytest
 
 
-def test_classification_proba_unsupervised():
+def test_score_samples_unsupervised():
     iris = datasets.load_iris()
     x = iris.data
     y = iris.target
@@ -14,9 +14,9 @@ def test_classification_proba_unsupervised():
 
     # Unsupervised model cannot classify
     with pytest.raises(Exception):
-        y_pred = ivis_iris.classification_proba(x)
+        y_pred = ivis_iris.score_samples(x)
 
-def test_classification_proba():
+def test_score_samples():
     iris = datasets.load_iris()
     x = iris.data
     y = iris.target
@@ -24,7 +24,7 @@ def test_classification_proba():
     ivis_iris = Ivis(k=15, batch_size=16, epochs=5)
 
     embeddings = ivis_iris.fit_transform(x, y)
-    y_pred = ivis_iris.classification_proba(x)
+    y_pred = ivis_iris.score_samples(x)
 
     # Softmax probabilities add to one, correct shape
     assert np.sum(y_pred, axis=-1) == pytest.approx(1, 0.01)
