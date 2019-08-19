0a46cb85da40df535796da5de2ebfc3e17fb0e35
Szubie <benszubert@gmail.com>
2019-08-19 12:38:17 +0100

    Add tests for new supervised functions (regression and SVM), as well as checking model saving when ivis was trained in supervised mode

--- tests/integration/test_supervised.py
+++ tests/integration/test_supervised.py
@@ -1,7 +1,9 @@
+from keras.datasets import boston_housing
+from keras.utils import to_categorical
 from ivis import Ivis
-from sklearn import datasets
 import numpy as np
 import pytest
+from sklearn import datasets
 
 
 def test_score_samples_unsupervised():
@@ -16,12 +18,15 @@ def test_score_samples_unsupervised():
     with pytest.raises(Exception):
         y_pred = ivis_iris.score_samples(x)
 
+
 def test_score_samples():
     iris = datasets.load_iris()
     x = iris.data
     y = iris.target
 
-    ivis_iris = Ivis(k=15, batch_size=16, epochs=5)
+    supervision_metric = 'sparse_categorical_crossentropy'
+    ivis_iris = Ivis(k=15, batch_size=16, epochs=5,
+                     supervision_metric=supervision_metric)
 
     embeddings = ivis_iris.fit_transform(x, y)
     y_pred = ivis_iris.score_samples(x)
@@ -30,3 +35,61 @@ def test_score_samples():
     assert np.sum(y_pred, axis=-1) == pytest.approx(1, 0.01)
     assert y_pred.shape[0] == x.shape[0]
     assert y_pred.shape[1] == len(np.unique(y))
+
+    # Check that loss function and activation are correct
+    assert ivis_iris.model_.loss['supervised'] == supervision_metric
+    assert ivis_iris.model_.layers[-1].activation.__name__ == 'softmax'
+
+
+def test_invalid_metric():
+    iris = datasets.load_iris()
+    x = iris.data
+    y = iris.target
+
+    supervision_metric = 'invalid_loss_function'
+    ivis_iris = Ivis(k=15, batch_size=16, epochs=5,
+                     supervision_metric=supervision_metric)
+
+    # Loss function not specified
+    with pytest.raises(ValueError):
+        embeddings = ivis_iris.fit_transform(x, y)
+
+
+def test_svm_score_samples():
+    iris = datasets.load_iris()
+    x = iris.data
+    y = iris.target
+
+    supervision_metric = 'categorical_hinge'
+    ivis_iris = Ivis(k=15, batch_size=16, epochs=5,
+                     supervision_metric=supervision_metric)
+
+    # Incorrectly formatted labels from SVM
+    with pytest.raises(ValueError):
+        embeddings = ivis_iris.fit_transform(x, y)
+
+    # Correctly formatted labels train successfully
+    y = to_categorical(y) * 2 - 1
+    embeddings = ivis_iris.fit_transform(x, y)
+
+    y_pred = ivis_iris.score_samples(x)
+    assert ivis_iris.model_.loss['supervised'] == supervision_metric
+    assert ivis_iris.model_.layers[-1].activation.__name__ == 'linear'
+    assert ivis_iris.model_.layers[-1].kernel_regularizer is not None
+    assert ivis_iris.model_.layers[-1].output_shape[-1] == y.shape[-1]
+
+
+def test_regression():
+    (x_train, y_train), (x_test, y_test) = boston_housing.load_data()
+
+    supervision_metric = 'mae'
+    ivis_boston = Ivis(k=15, batch_size=16, epochs=5,
+                       supervision_metric=supervision_metric)
+    ivis_boston.fit(x_train, y_train)
+
+    embeddings = ivis_boston.transform(x_train)
+    y_pred = ivis_boston.score_samples(x_train)
+
+    assert ivis_boston.model_.loss['supervised'] == 'mae'
+    assert ivis_boston.model_.layers[-1].activation.__name__ == 'linear'
+    assert ivis_boston.model_.layers[-1].output_shape[-1] == 1
