5ceee065766b7a1c2205f83b54f0bffef839ff04
Ignat Drozdov <idrozdov@beringresearch.com>
2020-01-10 19:20:29 +0000

    adjust conf for autodocs

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,9 @@
 #import os
 #import sys
 #sys.path.insert(0, os.path.abspath('../..'))
-
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
