45077e01d5c01e00e55bc0279f75f90e27dcec94
Ignat Drozdov <idrozdov@beringresearch.com>
2020-11-04 06:51:48 +0000

    add embeddings vis docstring

--- ivis/utils/vis_utils.py
+++ ivis/utils/vis_utils.py
@@ -39,6 +39,11 @@ def check_dependencies():
 
 
 class EmbeddingsExplorer(param.Parameterized):
+    """Interactive visualisation of embeddings.
+
+    Displays a scatterplot that can be embedded inside a Jupyter notebook.
+    """
+
     label_flag = param.Boolean(default=True)
 
     def __init__(self):
@@ -53,7 +58,7 @@ class EmbeddingsExplorer(param.Parameterized):
         self.classes = None
 
     @param.depends('label_flag')
-    def get_points(self):
+    def _get_points(self):
         embeddings = self.embeddings
         classes = self.classes
         if (self.label_flag) and (classes is not None):
@@ -77,6 +82,19 @@ class EmbeddingsExplorer(param.Parameterized):
         return points
 
     def view(self, embeddings, classes=None):
+        """Visualise embeddings
+
+        Parameters
+        ----------
+        embeddings : n-dimensional array shape (n_samples, )
+            Data to be visualised.
+        classes : array, shape (n_samples)
+            Optional array of class labels to be overlayed on the scatterplot.
+
+        Returns
+        -------
+        An instance of panel.Row
+        """
         self.embeddings = embeddings
         self.classes = classes
-        return pn.Row(self.get_points, self.param)
+        return pn.Row(self._get_points, self.param)
