31272eabe4cb40b7b49db97f6b63fc9c894a636e
Szubie <benszubert@gmail.com>
2021-06-17 14:39:18 +0100

    Add support to TripletGenerator class for calling batched retrieval methods on data sequences or neighbour indexes if these specialized methods exist. Separated triplet index generation from the actual retrieval of data using those indices. Added fast-path for cases where data is numpy ndarray, and a broadcasted version of negative indices retrieval when neighbour array is not ragged.

--- ivis/data/generators/triplet_generators.py
+++ ivis/data/generators/triplet_generators.py
@@ -8,6 +8,9 @@ dynamically generated.
 """
 
 from abc import ABC, abstractmethod
+import itertools
+import random
+import math
 import numpy as np
 from tensorflow.keras.utils import Sequence
 from scipy.sparse import issparse
@@ -23,50 +26,113 @@ def generator_from_neighbour_matrix(X, Y, neighbour_matrix, batch_size):
 class TripletGenerator(Sequence, ABC):
     def __init__(self, X, neighbour_matrix, batch_size=32):
         if batch_size > X.shape[0]:
-            raise Exception('''batch_size value larger than num_rows in dataset
-                            (batch_size={}, rows={}). Lower batch_size to a
-                            smaller value.'''.format(batch_size, X.shape[0]))
+            raise ValueError('''batch_size value larger than num_rows in dataset
+                             (batch_size={}, rows={}). Lower batch_size to a
+                             smaller value.'''.format(batch_size, X.shape[0]))
         self.X = X
         self.neighbour_matrix = neighbour_matrix
         self.batch_size = batch_size
+        self.batched_data = hasattr(X, 'get_batch')
+        self.batched_neighbours = hasattr(neighbour_matrix, 'get_batch')
 
     def __len__(self):
-        return int(np.ceil(self.X.shape[0] / float(self.batch_size)))
+        return int(math.ceil(self.X.shape[0] / float(self.batch_size)))
 
     def __getitem__(self, idx):
+        """Gets one batch of triplets"""
         batch_indices = range(idx * self.batch_size,
                               min((idx + 1) * self.batch_size, self.X.shape[0]))
-
+        triplet_indices = self.get_triplet_indices(batch_indices)
         label_batch = self.get_labels(batch_indices)
-        triplet_batch = [self.get_triplet(row_index)
-                         for row_index in batch_indices]
+
+        # Retrieve actual data using triplet_indices
+        if self.batched_data:
+            # Flatten triplets, get batch of data, then reshape back into triplets
+            indices = list(itertools.chain.from_iterable(triplet_indices))
+            data = self.X.get_batch(indices)
+            triplet_batch = list(zip(*[itertools.islice(data, i, None, 3) for i in range(3)]))
+        else:
+            if isinstance(self.X, np.ndarray):
+                # Fancy index for speed if data is a numpy array
+                triplet_indices = np.asarray(triplet_indices, dtype=np.array(self.X.shape[0]).dtype)
+                triplet_batch = self.X[np.moveaxis(triplet_indices, 0, 1)]
+            else:
+                triplet_batch = [(self.X[idx], self.X[pos_idx], self.X[neg_idx])
+                                 for (idx, pos_idx, neg_idx) in zip(*triplet_indices)]
 
         if issparse(self.X):
             triplet_batch = [[e.toarray()[0] for e in t] for t in triplet_batch]
 
-        triplet_batch = np.array(triplet_batch)
+        triplet_batch = np.asarray(triplet_batch)
 
         return self.output_triplets(triplet_batch, label_batch)
 
-    def get_triplet(self, idx):
-        triplet = []
-        neighbour_list = self.get_neighbours(idx)
-        neighbour_list = np.array(neighbour_list, dtype=np.uint32)
+    def get_triplet_indices(self, idx_seq):
+        """Generates and returns the triplet indices corresponding to the provided indexes.
+        Neighbours are randomly sampled for each row from self.neighbour_matrix and negative
+        samples that are not in the neighbour list are generated randomly."""
+        anchor_indices = np.fromiter(idx_seq,
+                                     dtype=np.array(self.X.shape[0]).dtype,
+                                     count=len(idx_seq))
+        neighbour_cands = self.get_all_neighbour_indices(idx_seq)
+        try:
+            # Auto ragged array creation deprecated in NumPy 1.19, 2019-11-01, will throw error
+            neighbour_cands = np.asarray(neighbour_cands)
+        except ValueError:
+            # Handle ragged array with slower, more generic method
+            return self.get_triplet_indices_generic(anchor_indices, neighbour_cands)
 
-        # Take a random neighbour as positive
-        neighbour_ind = np.random.choice(neighbour_list)
+        # Older numpy versions will autocast to dtype=object instead of raise exception
+        # Detect ragged array in this case and handle with generic method
+        if neighbour_cands.ndim < 2:
+            return self.get_triplet_indices_generic(anchor_indices, neighbour_cands)
 
-        # Take a random non-neighbour as negative
-        # Pick a random index until one fits constraint, usually faster.
-        negative_ind = np.random.randint(0, self.X.shape[0])
-        while negative_ind in neighbour_list:
-            negative_ind = np.random.randint(0, self.X.shape[0])
+        # Non-ragged array - use shape info to randomly select indices all at once
+        neighbour_indices = neighbour_cands[:, np.random.randint(neighbour_cands.shape[1])]
+        negative_indices = self.gen_negative_indices(neighbour_cands)
+        return (anchor_indices, neighbour_indices, negative_indices)
 
-        triplet += [self.X[idx], self.X[neighbour_ind], self.X[negative_ind]]
-        return triplet
+    def get_all_neighbour_indices(self, idx_seq):
+        """Retrieves neighbours for the indexes provided from inner neighbour matrix.
+        Uses specialized `get_batch` retrieval method if generator is in batched_neighbours mode.
+        """
+        if self.batched_neighbours:
+            return self.neighbour_matrix.get_batch(idx_seq)
+        return [self.neighbour_matrix[idx] for idx in idx_seq]
 
-    def get_neighbours(self, idx):
-        return self.neighbour_matrix[idx]
+    def gen_negative_indices(self, neighbour_matrix):
+        """Generate random candidate negative indices until the candidate for every
+        row is not present in corresponding row of neighbour_matrix."""
+        neighbour_matrix = np.asarray(neighbour_matrix)
+        cands = np.random.randint(self.X.shape[0], size=len(neighbour_matrix))
+
+        # Where random cand is present in neighbour row, invalid cand
+        invalid_cands = (cands[:, np.newaxis] == neighbour_matrix).any(axis=1)
+        n_invalid = invalid_cands.sum()
+        while n_invalid > 0:
+            cands[invalid_cands] = np.random.randint(self.X.shape[0], size=n_invalid)
+            invalid_cands = (cands[:, np.newaxis] == neighbour_matrix).any(axis=1)
+            n_invalid = invalid_cands.sum()
+        return cands
+
+    def get_triplet_indices_generic(self, anchor_indices, neighbour_cands=None):
+        """Slower, generic way of generating triplet indices that works on
+        sequences, not just numpy arrays."""
+        if neighbour_cands is None:
+            neighbour_cands = self.get_all_neighbour_indices(anchor_indices)
+
+        neighbour_indices = list(map(random.choice, neighbour_cands))
+        negative_indices = self.gen_negative_indices_generic(neighbour_cands)
+        return (np.asarray(anchor_indices), neighbour_indices, negative_indices)
+
+    def gen_negative_indices_generic(self, neighbour_map):
+        """Slower, generic way of generating negative indices that works on
+        sequences, not just numpy arrays."""
+        cands = [random.randrange(0, self.X.shape[0]) for i in range(len(neighbour_map))]
+        for i in range(len(cands)):
+            while cands[i] in neighbour_map[i]:
+                cands[i] = random.randrange(0, self.X.shape[0])
+        return cands
 
     @abstractmethod
     def get_labels(self, batch_indices):
