ebe6b297823effc5a567f7a4cad48dff02b90699
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2021-06-27 20:55:35 -0300

    Docstring added to `Ivis.utils.deprecation.check_deprecated_ntrees`

--- ivis/utils/deprecation.py
+++ ivis/utils/deprecation.py
@@ -2,6 +2,7 @@ import warnings
 
 
 def check_deprecated_ntrees(ntrees):
+    """ Checks for the use of ntrees """
     if ntrees is not None:
         warnings.warn("Received a value for `ntrees`. In the future, passing "
                       "a value to `ntrees` will result in a ValueError. "
