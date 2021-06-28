5df366e615ff63f0de7252b2a3fb71f637634e45
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2021-06-27 21:05:07 -0300

    Deprecation verification moved to `Ivis._check_parameters()`
    All logic was moved from `Ivis.__init__()`. This solution is still not ideal, for it changes the input hyper-parameters during execution; however, it might be acceptable considering that the user is warned of such change and that this behavior is temporary.

--- ivis/utils/deprecation.py
+++ ivis/utils/deprecation.py
@@ -6,5 +6,5 @@ def check_deprecated_ntrees(ntrees):
     if ntrees is not None:
         warnings.warn("Received a value for `ntrees`. In the future, passing "
                       "a value to `ntrees` will result in a ValueError. "
-                      "`n_trees` was set to `ntrees`. Use `n_trees` "
+                      f"`n_trees` was set to `{ntrees}`. Use `n_trees` "
                       "instead.", FutureWarning)
